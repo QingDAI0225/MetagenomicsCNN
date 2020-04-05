a977abe346c65849baeb55639826b78cc5f68135
Victor <victor.wang647@hotmail.com>
2020-04-05 14:01:42 -0400

    added flexible kmer encoding

--- read2arrary/read2array.py
+++ read2arrary/read2array.py
@@ -10,13 +10,15 @@ Workshops at the Twenty-Ninth AAAI Conference on Artificial Intelligence. 2015.
 import sys
 import numpy as np
 
-def read2array(read, array_type='GAF'):
+def read2array(read, kmer_length=1, array_type='GAF'):
     """Converts a string into an array.
 
     Parameters
     ----------
     read : str
         A read from shotgun sequencing. Bases must be A, C, G, or T.
+    kmer_length: int
+        Length of kmers used for encoding read into base
     array_type : 'GAF' or 'MTF'
         Specifies type of array the read will be encoded in.
         'GAF' denotes Gramian Angular Field
@@ -28,14 +30,43 @@ def read2array(read, array_type='GAF'):
     """
 
     # encode read as 1D array of integers (time series)
-    nt2int = {'A':0, 'C':1, 'G':2, 'T':3}
-    time_series = np.array([nt2int[b] for b in read])
+    time_series = read2num(read,kmer_length)
 
     if array_type == 'GAF':
         array = getGAF(time_series)
 
     return array
 
+def read2num(read,kmer_length):
+    """Converts the read into a 1D array of numbers
+    Parameters
+    ----------
+    read : str
+        A read from shotgun sequencing. Bases must be A, C, G, or T.
+    kmer_length: int
+        Length of kmers used for encoding read into base
+
+    Returns
+    -------
+    numpy.array
+    """
+
+    num_kmers = len(read) - kmer_length + 1
+    nt2int = {'A':'0', 'C':'1', 'G':'2', 'T':'3'}
+    time_series = []
+    for i in range(num_kmers):
+
+        # split into kmers
+        kmer = read[i:i+kmer_length]
+
+        # convert to base 4 num
+        kmer_base4 = "".join([nt2int[b] for b in kmer])
+
+        # convert to base 10 number
+        kmer_num = int(kmer_base4,base=4)
+        time_series.append(kmer_num)
+
+    return np.array(time_series)
 
 def getGAF(x):
     """Converts time series into GAF
@@ -79,4 +110,4 @@ def getGAF(x):
 
 if __name__ == "__main__":
     read = sys.argv[1]
-    print(read2array(read))
+    print(read2array(read,kmer_length=3))
